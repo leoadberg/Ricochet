08f28ca112f47c4d5d54af10c07266ad3aedca77
Leo Adberg <leo@adberg.com>
2016-04-03 00:47:37 -0700

    Began implementing class structure to level effects

---
+++ Ricochet/LevelEffect.swift
@@ -0,0 +1,21 @@
+//
+//  LevelEffect.swift
+//  Ricochet
+//
+//  Created by Leo Adberg on 4/3/16.
+//  Copyright © 2016 Tigersushi. All rights reserved.
+//
+
+import Foundation
+
+class LevelEffect {
+    
+    var effectID = -1
+    
+    init(effectID: Int) {
+        self.effectID = effectID
+    }
+    
+    func update(scene: GameScene, timeSinceLastUpdate: Double) {}
+    
+}
