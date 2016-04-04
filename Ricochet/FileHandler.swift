1dadebdcdaf7bfcb2b038872ab6624d165a63c33
Brandon <Brandon@s-128-97-168-75.resnet.ucla.edu>
2016-04-03 22:46:42 -0700

    fixed some formatting and added collisions to resizing obstacles

--- Ricochet/FileHandler.swift
+++ Ricochet/FileHandler.swift
@@ -10,23 +10,28 @@ import Foundation
 import UIKit
 
 func CreateArrayFromPlist(plist: String, userDomain: Bool) -> NSMutableArray {
+    
     var path: String = ""
+    
     if (userDomain) {
         path = (NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String).stringByAppendingString(plist+".plist")
     }
     else {
         path = NSBundle.mainBundle().pathForResource(plist, ofType: "plist")!
     }
+    
     if (!FILEMANAGER.fileExistsAtPath(path)) {
         //let emptyArray: [Level] = []
         let empty = NSMutableArray()
         return empty
         //FILEMANAGER.createFileAtPath(path, contents: nil, attributes: nil)
     }
+    
     return NSMutableArray(contentsOfFile: path)!
 }
 
 func CreateLevelWithProperties(levelNumber: Int, dict: NSDictionary) -> Level {
+    
     let tempLevel = Level(level: levelNumber)
     tempLevel.hint = dict["Hint"] as! String
     tempLevel.mode = dict["Game Mode"] as! Int
@@ -42,16 +47,21 @@ func CreateLevelWithProperties(levelNumber: Int, dict: NSDictionary) -> Level {
     let tempEffects = dict["Effects"] as! NSArray
     for effect in tempEffects {
         switch (effect["EffectID"] as! Int) {
+            
         case 0:
             tempLevel.effects.append(DirectionalGravity(x: effect["Gravity X"] as! Double, y: effect["Gravity Y"] as! Double))
-            break;
+            
         case 1:
             tempLevel.effects.append(PointGravity(x: effect["Gravity X"] as! Double, y: effect["Gravity Y"] as! Double, str: effect["Gravity Strength"] as! Double))
+            
         case 2:
             tempLevel.effects.append(RotatingObstacle(rate: effect["Rotation Rate"] as! Double))
+            
         default:
-            break;
+            break
+            
         }
     }
+    
     return tempLevel
 }
