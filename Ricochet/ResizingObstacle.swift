4caba95ba00e179bda6b982805b07f7aa2732e53
Brandon <Brandon@s-128-97-168-75.resnet.ucla.edu>
2016-04-03 22:27:28 -0700

    Added ResizingObstacle and RotatingObstacle class

---
+++ Ricochet/ResizingObstacle.swift
@@ -0,0 +1,38 @@
+//
+//  ResizingObstacle.swift
+//  Ricochet
+//
+//  Created by Brandon on 4/3/16.
+//  Copyright © 2016 Tigersushi. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class ResizingObstacle: LevelEffect {
+    
+    var rate: Double = 0.0
+    var maxScale: Double = -1.0
+    var minScale: Double = -1.0
+    var scale: Double = 1.0
+    
+    init(rate: Double, maxScale: Double, minScale: Double) {
+        
+        self.rate = rate
+        self.maxScale = maxScale
+        self.minScale = minScale
+        
+    }
+    
+    override func update(timeSinceLastUpdate: Double, inout _ ball: Ball, inout _ obstacle: Obstacle) {
+        
+        scale += self.rate * timeSinceLastUpdate
+        obstacle.setScale(CGFloat(self.scale))
+        
+        if (rate > 0 && self.scale > self.maxScale || rate < 0 && self.scale < self.minScale) {
+            rate *= -1
+        }
+        
+    }
+    
+}
